60c958582c433cae72b69b58334a4b9ab7301169
Arimeyaw Bartah Mohammed <tymermohammed77u@gmail.com>
2026-02-18 23:07:56 +0000

    arrays-type

--- app.ts
+++ app.ts
@@ -1,15 +1,19 @@
-let userInput: unknown;
-let userName: string;
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+const person = {
+  name: "Maximilian",
+  age: 30,
+  hobbies: ["Sports", "Cooking"],
+};
 
-userInput = 5;
-userInput = "Max";
-if (typeof userInput === "string") {
-  userName = userInput;
+let favoriteActivities: string[];
+favoriteActivities = ["Sports"];
+
+console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+  // console.log(hobby.map()); // !!! ERROR !!!
 }
-
-function generateError(message: string, code: number): never {
-  throw { message: message, errorCode: code };
-  // while (true) {}
-}
-
-generateError("An error occurred!", 500);
